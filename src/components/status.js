088f79691e56643ce7766bdbd3262d146ddcde17
Xnths <jonathascastilhoes@gmail.com>
2024-01-28 18:50:36 -0300

    feat: Construction of status component

---
+++ src/components/status.js
@@ -0,0 +1,18 @@
+import React from "react";
+import { CreditBar } from "../atomic/creditBar";
+
+export const Status = () => {
+    return (
+        <div style={{
+            display: "flex",
+            justifyContent: "space-around",
+            width: "100vw",
+            backgroundColor: "#F5F5F5",
+            height: "40vh",
+        }}>
+            <CreditBar creditType="Obrigatória" color="#0D75FD" creditEarned="111" creditRequired="111" />
+            <CreditBar creditType="Eletiva" color="#00D0FF" creditEarned="56" creditRequired="56" />
+            <CreditBar creditType="Livre" color="#00E1CF" creditEarned="24" creditRequired="24" />
+        </div>
+    )
+}
